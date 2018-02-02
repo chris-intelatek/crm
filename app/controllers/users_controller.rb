092a9eb2bb9cf80b60e2c4d005285d9599b9a61d
Chris Cunningham <chris@intelatek.com>
2018-02-02 03:00:53 +0000

    add user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,33 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!, :except => [:show]
 
   def index
+  	@users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+  
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
+  
+  def update
+  	@user = User.find_by_id(params[:id])
+	if @user.update_attributes(user_params)
+		redirect_to users_path
+	else
+		render 'edit'
+	end
+  end
+
+  def user_params
+    params.require(:user).permit(:advisor_first_name, :advisor_last_name, :advisor_phone, :advisor_mobile, :admin, :manager, :email, :password, :password_confirmation)
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130041927) do
+ActiveRecord::Schema.define(version: 20180202003724) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20180130041927) do
     t.string   "contact2_mobile"
     t.string   "contact2_email"
     t.integer  "primary_contact"
+    t.string   "intro_presenter"
+    t.string   "sm_presenter"
   end
 
   create_table "users", force: :cascade do |t|
