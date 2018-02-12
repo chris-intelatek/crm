c36991021ef03fe6ccd256bb97e0bc5c00c30eb3
Chris Cunningham <chris@intelatek.com>
2018-02-12 19:59:53 +0000

    add user count and user.prospect size

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :prospects
-  has_many :comments
+  has_many :prospects, :dependent => :delete_all
+  has_many :comments, :dependent => :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
